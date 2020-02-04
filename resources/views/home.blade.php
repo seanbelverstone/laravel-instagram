33dc2ad69ec99c91a6c740fab4ae04604c95ad1b
Sean Belverstone <seanbelverstone@gmail.com>
2020-02-03 19:17:13 -0600

    Made some changes. Changed the profile to be CatGang instead and changed the picture. Also changed the background to be full white instead of a blueish white.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,15 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://image.flaticon.com/icons/svg/733/733558.svg" alt="instaimage">
+            <img src="https://i.redd.it/05gaxvob1gd21.jpg" alt="catgang" style="height: 200px; border: 1pt solid black" class="rounded-circle">
         </div>
-        <div class="col-3">
-            <div><h1>Instagram Laravel</h1></div>
-            <div><strong>153</strong> posts</div>
-            <div></div>
+        <div class="col-9 pt-5">
+            <div><h1>Cat Gang</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>153</strong> posts</div>
+                <div class="pr-4"><strong>23k</strong> followers</div>
+                <div class="pr-4"><strong>212</strong> following</div>
+            </div>
             <div></div>
         </div>
     </div>
