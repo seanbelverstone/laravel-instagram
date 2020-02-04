aa59bcaa29ecb32c9e24dc2862ad2923c05ad333
Sean Belverstone <seanbelverstone@gmail.com>
2020-02-03 21:34:45 -0600

    Finished top section. Added in images complete with padding and rows

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,8 +15,23 @@
                 <div class="pr-4"><strong>23k</strong> followers</div>
                 <div class="pr-4"><strong>212</strong> following</div>
             </div>
-            <div></div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>Cats are the best! Wanna join our gang?</div>
+            <div><a href="#">www.catgang.com</a></div>
         </div>
     </div>
+
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://external-preview.redd.it/Ml-xR0f_NHdJjFw8FncFLY9ZJGy4XAdZbcEYnNd_P1g.jpg?auto=webp&s=f43b46a61617a86ba6b23312fccf173bd5e8434c" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://i.redd.it/fotvn23fo0e21.jpg" alt="" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://i.pinimg.com/736x/5c/07/44/5c07441f38d6aa0a1f4f3fdecfa85879.jpg" alt="" class="w-100">
+        </div>    
+    </div>    
 </div>
 @endsection
