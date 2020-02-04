a8c8adf8d2209369413f941a1f1e83d7e469f9d9
Sean Belverstone <seanbelverstone@gmail.com>
2020-02-03 18:53:01 -0600

    Removed code from home.blade.php and started adding in the custom design for the instagram replica

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,21 +4,15 @@
 
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://image.flaticon.com/icons/svg/733/733558.svg" alt="instaimage">
+        </div>
+        <div class="col-3">
+            <div><h1>Instagram Laravel</h1></div>
+            <div><strong>153</strong> posts</div>
+            <div></div>
+            <div></div>
         </div>
     </div>
 </div>
